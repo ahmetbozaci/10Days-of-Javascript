1dd682b7ced1cb88286ae87f2e749ad968ea02a3
Ahmet <ahmt9417@gmail.com>
2021-10-31 18:33:43 +0300

    Solve day-3 challenges

---
+++ 3-Day.js
@@ -0,0 +1,29 @@
+//! Arrays
+function getSecondLargest(nums) {
+  let delDublicate = [...new Set(nums)];
+  let newArray = delDublicate.sort((a, b) => b - a);
+  return newArray[1];
+}
+
+//! Try, Catch and Finally
+function reverseString(s) {
+  try {
+    let array = s.split('');
+    let reverseString = array.reverse().join('');
+    console.log(reverseString);
+  } catch (error) {
+    console.log(error.message);
+    console.log(s);
+  }
+}
+
+//! Throw
+function isPositive(a) {
+  if (a > 0) {
+    return 'YES';
+  } else if (a < 0) {
+    throw new Error('Negative Error');
+  } else {
+    throw new Error('Zero Error');
+  }
+}
